3dbd7f3b4363a2f72c4758f2c8ac98708262a367
petter h <dr.doddo@gmail.com>
2014-04-30 00:11:33 +0200

    color stub

---
+++ Gutta/Color.pm
@@ -0,0 +1,24 @@
+package Gutta::Color;
+
+# enables colored output.
+#
+
+
+my $White = "0";
+my $Black = "1";
+my $Blue = "2";
+my $Green = "3";
+my $LightRed = "4";
+my $Brown = "5";
+my $Purple = "6";
+my $Orange = "7";
+my $Yellow = "8";
+my $LightGreen = "9";
+my $Cyan = "10";
+my $LightCyan = "11";
+my $LightBlue = "12";
+my $Pink = "13";
+my $Grey = "14";
+my $LightGrey = "15";
+
+1;
