235e1b466dce183f92680baa0453f4766dea6829
Petter H <dr.doddo@gmail.com>
2014-07-03 22:39:49 +0200

    added some tests

---
+++ Gutta/Tests/Plugins.pm
@@ -0,0 +1,30 @@
+package Gutta::Tests::Plugins;
+
+use Pod::Usage;
+
+use Test::More tests => 20;
+
+use strict;
+use warnings;
+
+
+
+# Instantiate all the plugins.
+use Module::Pluggable search_path => "Gutta::Plugins",
+                          require => 1;
+#
+
+
+
+foreach my $plugin (plugins())
+{
+
+    $plugin->new();
+
+    ok( defined $plugin, "Test if  $plugin  defined..." );
+    ok( $plugin->isa('Gutta::Plugin'), "Test if $plugin is a Gutta::Plugin..." ); 
+
+}
+
+
+1;
