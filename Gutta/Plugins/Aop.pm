13d3dc4b2e875600bd72b8995bce33bc67b07a99
Petter H <dr.doddo@gmail.com>
2014-09-17 23:07:46 +0200

    fixed some more on aop plugin

--- Gutta/Plugins/Aop.pm
+++ Gutta/Plugins/Aop.pm
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Switch;
+use Getopt::Long qw(GetOptionsFromArray);
 my $log = Log::Log4perl->get_logger(__PACKAGE__);
 
 =head1 NAME
@@ -27,7 +28,7 @@ upon request.
 Handle aop:ing the incoming nick. aop:s get op.
 
 
-  !aop [ add | del | modify ] NICK [ --channel C ] [ --level INT ] [ --mask M ]
+  !aop [ info | [ add | del | modify ] NICK [ --channel C ] [ --lvl INT ] [ --mask M ]] 
 
 =over 8
 
@@ -43,8 +44,8 @@ than itself has. A number above 1 means that the user will be oped, (but can't o
 =item B<--mask>
 
 If the bot doesn't know what hostmask is associated with a nick, or the user
-has not registered with register command, the mask must be manually sent to bot,
-with the --mask opt.
+has not registered with register command and is identified, the mask must be 
+manually sent to bot, with the --mask opt.
 
 =back
 
@@ -58,18 +59,29 @@ If is omitted, it will assume current channel.
 
 =cut
 
+sub _initialise
+{
+    # THe constructor in the parent class calls for this.
+    my $self = shift;
+
+    $self->{ session } = Gutta::Session->instance();
+    $self->_dbinit(); 
+}
+
+
 sub _setup_shema
 {
     my $self = shift;
 
     my @queries  = (qq{
     CREATE TABLE IF NOT EXISTS aops (
-            channel TEXT NOT NULL,
                nick TEXT NOT NULL,
                mask TEXT NOT NULL,
+            channel TEXT NOT NULL,
+                lvl INTEGER,
     FOREIGN KEY(nick) REFERENCES users(nick),
       CONSTRAINT nick_per_chan UNIQUE (nick, channel)
-    });
+    )});
 
     return @queries;
 }
@@ -89,7 +101,8 @@ sub _commands
     my $self = shift;
     return {
 
-        'aop' => sub { $self->process_cmd(@_) },
+        'aop' => sub { $self->process_cmd('aop', @_) },
+         'op' => sub { $self->process_cmd('op', @_) },
 
     }
 }
@@ -107,13 +120,124 @@ sub on_join
     return "";
 }
 
-sub process_msg
+sub process_cmd
 {
     my $self = shift;
+    my $cmd = shift;
+    my $server = shift;
+    my $msg = shift;
+    my $nick = shift;
+    my $mask = shift;
+    my $target = shift;
+    my $rest_of_msg = shift;
 
+    # some vars used ...
+    my @responses;
+    my @rest_of_msg;
+    my $subcmd;
+    my $target_nick;
 
-    return "";
+    # Possble subcmd...
+    if ($rest_of_msg)
+    {
+        ($subcmd, @rest_of_msg) = split(' ',$rest_of_msg);
+        if (@rest_of_msg)
+        {
+            $target_nick = shift(@rest_of_msg);
+        }
+    }
+
+    # OK do they want OP?
+    if ($cmd eq 'op')
+    {
+        $self->__handle_op($nick, $mask, $target, @rest_of_msg);
+    } elsif ($cmd eq 'aop'){
+        if ($subcmd ~~ ["add", "del", "modify"])
+        {
+            push @responses, 
+                $self->__nickmod($subcmd, $nick, $mask, $target, $target_nick, @rest_of_msg);
+        }
+    }
+
+    return @responses;
 }
 
+sub __handle_op
+{
+    my $self = shift;
+    my $nick = shift;
+    my $mask = shift;
+    my $target = shift;
+    my @rest_of_msg = @_;
+
+    # TODO
+
+    return ;
+
+}
+
+sub __nickmod
+{
+    # Adds a nick to be automatically op:ed ...
+    my $self = shift;
+    my $subcmd = shift;
+    my $nick = shift;
+    my $mask = shift;
+    my $target = shift;
+    my $target_nick = shift;
+    my @args = @_; 
+    
+    # Parse options...
+    my $channel;
+    my $level;
+    my $target_mask;
+    my $lvl;
+
+    GetOptionsDromArray(\@args,
+          'channel=s' => \$channel,
+              'lvl=i' => \$lvl,
+             'mask=s' => \$target_mask)
+    or return "invalid options supplied";
+
+    #
+    # Has the user logged in to the system?
+    #
+    return "msg $target please identify first." unless $self->has_session($nick, $mask); 
+
+    # Validate lvl if there is one.
+    if ($lvl && ($lvl < 0 || $lvl > 100))
+    {
+        return "msg $target invalid lvl supplied:$lvl, should be between 0-100";
+    }
+    
+    # Figure out who is the target
+    unless ($target_nick)
+    {
+        return "msg $target missing target nick.";
+    }
+
+    # Figure out in what channel to target.
+    if ($channel)
+    {
+        unless ($channel =~ m/^#+[a-z_0-9]{1,100}\b/i)
+        {
+            return "msg $target '$channel' does not validate as channel";
+            $log->info("${nick} supplied an invalid channel '${channel}'...");
+        }
+
+    } elsif ($target =~ m/^#/){
+        $log->debug("No --channel option supplied, falling back to $target...");
+        $channel = $target;
+    } else {
+        # Bot does not know what channels this is about.
+        return "msg $target unable to compute; don't know what --channel...";
+    }
+
+    # Gather system information about $nick.
+   
+
+
+    
+}
 
 1;
